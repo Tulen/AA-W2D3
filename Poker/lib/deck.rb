5a813a19afe9c6fc4111ac3cdb0807b025e6087f
Spencer Chan <schan1031@gmail.com>
2017-06-07 15:29:12 -0700

    initial commit, cards and deck

---
+++ Poker/lib/deck.rb
@@ -0,0 +1,27 @@
+require 'card'
+
+class Deck
+  SUITS = [:d, :c, :h, :s].freeze
+  VALUES = (2..14).to_a.freeze
+
+  attr_reader :deck_list
+
+  def initialize
+    @deck_list = []
+    populate
+    shuffle!
+  end
+
+  def populate
+    SUITS.each do |suit|
+      VALUES.each do |val|
+        @deck_list << Card.new(val, suit)
+      end
+    end
+    @deck_list
+  end
+
+  def shuffle!
+    @deck_list.shuffle!
+  end
+end
